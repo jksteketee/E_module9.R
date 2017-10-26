f9e43cbbb4643fab712ada8e1f3fbc374ee59296
Jess Steketee <jess.steketee13@gmail.com>
2017-10-25 20:16:30 -0400

    Committing what I have so Kier can see

--- E_module9.R
+++ E_module9.R
@@ -9,5 +9,17 @@ data("msleep")
 head(msleep)
 
 #1. How many different diet types are there? 
+#Make a boxplot comparing the daily sleep totals across the vore categories, excluding rows that 
+#do not have data in the vore column. Remember to include informative axis labels. 
+#Briefly describe in 1-2 sentences the major patterns in the plot.
+
 length(unique(msleep$vore))
-#There are 5 different types
+length(unique(msleep$vore))
+
+#There are 4 different diet types: Carni, omni, herbi, insecti and there are some NA's as well. 
+#Boxplot: 
+
+
+
+
+ggplot(bar.sum, aes(x=))
